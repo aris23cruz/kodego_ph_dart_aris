70525ad16c21221f424d5a3fc41a941fed2d70a0
aris cruz <ariscruz.va@gmail.com>
2023-01-07 10:11:08 +0800

    4th commit

--- src/dart_program12.dart
+++ src/dart_program12.dart
@@ -1,6 +1,14 @@
+import 'dart:io';
 void main(){
   var users = {"1":"user1", "2":"user2"};
-  print(users["1"]);
+  // print(users["1"]);
+  // print(users["2"]);
+  String key;
+
+  print("Enter Key: ");
+  key = stdin.readLineSync()!;
+
+  print(users[key]);
   print(users["2"]);
 
   print(users);
